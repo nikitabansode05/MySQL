f3d7f922df1c0ad6fb63314381f9fe484d50972f
Nikita Bansode <bansodenikita66@gmail.com>
2025-10-14 09:10:52 +0530

    changes

--- StoredProcedure/productReview.sql
+++ StoredProcedure/productReview.sql
@@ -4,7 +4,6 @@ DELIMITER //
 CREATE PROCEDURE productReview(p_product_id INT,p_user_id INT,p_rating INT,p_review VARCHAR(50))
 	BEGIN
 		INSERT INTO reviews(product_id,user_id,rating,review_text) VALUES(p_product_id,p_user_id,p_rating,p_review);
-        
     END //  
 DELIMITER ;
 
